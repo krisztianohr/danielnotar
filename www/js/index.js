50b9075f852dde22ca3b96de14181c24a980bf91
Ohr Krisztian <ohrkrisztian@iMac.local>
2013-12-31 09:58:57 +0100

    index 1.9.2

--- www/js/index.js
+++ www/js/index.js
@@ -39,23 +39,9 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 
-        var networkState = navigator.connection.type;
-
-        var states = {};
-        states[Connection.UNKNOWN]  = 'Unknown connection';
-        states[Connection.ETHERNET] = 'Ethernet connection';
-        states[Connection.WIFI]     = 'WiFi connection';
-        states[Connection.CELL_2G]  = 'Cell 2G connection';
-        states[Connection.CELL_3G]  = 'Cell 3G connection';
-        states[Connection.CELL_4G]  = 'Cell 4G connection';
-        states[Connection.NONE]     = 'No network connection';
+        var networkState = navigator.onLine;
 
 		alert('Connection type: ' + states[networkState]);
-		
-		navigator.globalization.getPreferredLanguage(
-		    function (language) {alert('language: ' + language.value + '\n');},
-		    function () {alert('Error getting language\n');}
-		);		
                
     },
 
