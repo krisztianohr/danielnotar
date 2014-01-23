c36f3e767ccac0c6f2a4d32455144bdf805bd9ef
Ohr Krisztian <ohrkrisztian@iMac.local>
2014-01-23 12:15:31 +0100

    APN 3

--- www/js/index.js
+++ www/js/index.js
@@ -2,61 +2,40 @@ var randomnumber = Math.floor(Math.random()*11000000);
 var appLanguage = "hu";
 var pushNotification;
 
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {    	        
-		checkLanguage();
-		
-		if( checkConnection() ) {
-			app.receivedEvent('deviceready');
+// deviceready Event Handler
+//
+// The scope of 'this' is the event. In order to call the 'receivedEvent'
+// function, we must explicity call 'app.receivedEvent(...);'
+function onDeviceReady() {
+	try {
+		pushNotification = window.plugins.pushNotification;		
+		pushNotification.register(
+			tokenHandler,
+			errorHandler, {
+				"badge":"true",
+				"sound":"true",
+				"alert":"true",
+				"ecb":"onNotificationAPN"
+		});
+	} catch(err) { 
+		txt="There was an error on this page.\n\n"; 
+		txt+="Error description: " + err.message + "\n\n"; 
+		alert(txt); 
+	}		
+	
+	checkLanguage();
+	
+	if( checkConnection() ) {
+		//app.receivedEvent('deviceready');
+		//var ref = window.open('http://app.danielnotar.com/?appLanguage=' + appLanguage + '&r=' + randomnumber, '_self', 'location=no,enableViewportScale=yes');
+	} else {
+		if (appLanguage == "hu") {
+			document.getElementById("startApp").innerHTML = "Nem tal&aacute;lhat&oacute; internet kapcsolat.<br>K&eacute;rem kapcsolja be az internetet &eacute;s pr&oacute;b&aacute;lja &uacute;jra.";
 		} else {
-			if (appLanguage == "hu") {
-				document.getElementById("startApp").innerHTML = "Nem tal&aacute;lhat&oacute; internet kapcsolat.<br>K&eacute;rem kapcsolja be az internetet &eacute;s pr&oacute;b&aacute;lja &uacute;jra.";
-			} else {
-				document.getElementById("startApp").innerHTML = "No internet connection detected!<br>Please turn on internet and try again.";
-			}
-		}                    
-    },
-
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {		
-
-		try {
-			pushNotification = window.plugins.pushNotification;		
-			pushNotification.register(
-				tokenHandler,
-				errorHandler, {
-					"badge":"true",
-					"sound":"true",
-					"alert":"true",
-					"ecb":"onNotificationAPN"
-			});
-		} catch(err) { 
-			txt="There was an error on this page.\n\n"; 
-			txt+="Error description: " + err.message + "\n\n"; 
-			alert(txt); 
+			document.getElementById("startApp").innerHTML = "No internet connection detected!<br>Please turn on internet and try again.";
 		}
-		
-        //var ref = window.open('http://app.danielnotar.com/?appLanguage=' + appLanguage + '&r=' + randomnumber, '_self', 'location=no,enableViewportScale=yes');
-    } 
-    
-};
+	}                    
+}
 
 // PUSH subs
 // result contains any message sent from the plugin call
@@ -113,3 +92,5 @@ function checkLanguage() {
 	);
 	appLanguage = appLanguage.substr(0, 2);
 }
+
+document.addEventListener('deviceready', onDeviceReady, true);
