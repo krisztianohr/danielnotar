61fe355d857e3c23781c4c9b54b159763d6d1513
Ohr Krisztian <ohrkrisztian@iMac.local>
2014-01-25 10:30:27 +0100

    child 1

--- www/js/index.js
+++ www/js/index.js
@@ -43,24 +43,27 @@ var app = {
 		
 		if( checkConnection() ) {
 			app.receivedEvent('deviceready');
+						
+			if (devicetoken != "") {
+				$("#cms-root").load(
+					"http://dev.itworx.hu/mobile/apn_token.php",
+					{
+						appID: "com.webmark.danielnotar",
+						token: devicetoken,
+						r: randomnumber
+					},
+					function(r) {
+						alert(r);
+					}
+				);
+			}
 			
-			appWindow = window.open(encodeURI('http://app.danielnotar.com/?appLanguage=' + appLanguage + '&r=' + randomnumber), '_self', 'location=no,enableViewportScale=yes,suppressesIncrementalRendering=yes,presentationstyle=fliphorizontal');
-			appWindow.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
+			//appWindow = window.open(encodeURI('http://app.danielnotar.com/?appLanguage=' + appLanguage + '&r=' + randomnumber), '_self', 'location=no,enableViewportScale=yes,suppressesIncrementalRendering=yes,presentationstyle=fliphorizontal');
+			//appWindow.addEventListener('loaderror', function(event) { alert('error: ' + event.message); });
 			
-			$("#cms-root").load(
-				"http://dev.itworx.hu/mobile/apn_token.php",
-				{
-					appID: "com.webmark.danielnotar",
-					token: result,
-					r: randomnumber
-				},
-				function(r) {
-					alert(r);
-				}
-			);
+			window.plugins.childBrowser.showWebPage('http://app.danielnotar.com/?appLanguage=' + appLanguage + '&r=' + randomnumber, { showLocationBar: false, showAddress: false, showNavigationBar: false });
 			
-			alert("loaded");
-			
+			alert("loaded");			
 		} else {
 			if (appLanguage == "hu") {
 				document.getElementById("startApp").innerHTML = "Nem tal&aacute;lhat&oacute; internet kapcsolat.<br>K&eacute;rem kapcsolja be az internetet &eacute;s pr&oacute;b&aacute;lja &uacute;jra.";
